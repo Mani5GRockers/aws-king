65ab6e7a29faab690417b9b5bb93893cfd3c207d
S M Mahabub Hossain <77372461+iamSMMH@users.noreply.github.com>
2022-01-08 18:53:47 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -112,29 +112,6 @@ def get_progress_bar_string(status):
     p_str = f"[{p_str}]"
     return p_str
 
-def progress_bar(percentage):
-    """Returns a progress bar for download
-    """
-    #percentage is on the scale of 0-1
-    comp = FINISHED_PROGRESS_STR
-    ncomp = UNFINISHED_PROGRESS_STR
-    pr = ""
-
-    if isinstance(percentage, str):
-        return "NaN"
-
-    try:
-        percentage=int(percentage)
-    except:
-        percentage = 0
-
-    for i in range(1,11):
-        if i <= int(percentage/10):
-            pr += comp
-        else:
-            pr += ncomp
-    return pr
-
 def get_readable_message():
     with download_dict_lock:
         msg = ""
