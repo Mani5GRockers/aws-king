f5b2a0efe76e738769a4bcd24821e31fe9fb202d
S M Mahabub Hossain <77372461+iamSMMH@users.noreply.github.com>
2022-01-13 17:53:48 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,11 @@
 import logging
-import socket
+import os
+import threading
+import time
+import random
+import string
+import subprocess
+import requests
 import faulthandler
 import aria2p
 import qbittorrentapi as qba
@@ -100,6 +106,7 @@ get_client().application.set_preferences({"add_trackers":f"{trackerslist}"})
 
 DOWNLOAD_DIR = None
 BOT_TOKEN = None
+BOT_NO = ""
 
 download_dict_lock = Lock()
 status_reply_dict_lock = Lock()
@@ -141,6 +148,10 @@ try:
         SUDO_USERS.add(int(chats))
 except:
     pass
+try:
+    BOT_NO = getConfig('BOT_NO')
+except KeyError:
+    BOT_NO = ""
 try:
     BOT_TOKEN = getConfig('BOT_TOKEN')
     parent_id = getConfig('GDRIVE_FOLDER_ID')
