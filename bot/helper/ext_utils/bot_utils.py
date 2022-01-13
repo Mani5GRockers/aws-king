ef495ca3f6b30e30f28faa7690c081073f4329cc
S M Mahabub Hossain <77372461+iamSMMH@users.noreply.github.com>
2022-01-13 18:50:36 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -10,7 +10,8 @@ from telegram.ext import CallbackQueryHandler
 
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot import dispatcher, download_dict, download_dict_lock, STATUS_LIMIT, botStartTime
-from bot.helper.telegram_helper import button_build, message_utils
+from bot.helper.telegram_helper import message_utils
+from bot.helper.telegram_helper.button_build import ButtonMaker
 
 MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
 
