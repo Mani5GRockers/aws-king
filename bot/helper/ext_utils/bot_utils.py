7199cbb1c9b49a59e292913bfaf62fed76c5eb26
S M Mahabub Hossain <77372461+iamSMMH@users.noreply.github.com>
2022-01-14 09:08:04 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -9,7 +9,7 @@ from telegram import InlineKeyboardMarkup
 from telegram.ext import CallbackQueryHandler
 
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot import dispatcher, download_dict, download_dict_lock, STATUS_LIMIT, botStartTime
+from bot import dispatcher, download_dict, download_dict_lock, STATUS_LIMIT, FINISHED_PROGRESS_STR, UNFINISHED_PROGRESS_STR, botStartTime
 from bot.helper.telegram_helper import message_utils
 from bot.helper.telegram_helper.button_build import ButtonMaker
 
